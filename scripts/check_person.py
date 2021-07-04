2db7276d3158f85fdd33c6905bb63ad510554b96
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2021-07-05 03:52:40 +0700

    ADD fix issues with check_person.py

--- scripts/check_person.py
+++ scripts/check_person.py
@@ -3,9 +3,9 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 # config variables
-IMAGE_LOCATION_KNOWN = "../images/aitemir_suit.jpg"
-IMAGE_LOCATION_UNKNOWN = "../images/almaz_bike.jpg"
-# IMAGE_LOCATION_UNKNOWN = "../images/almazer.jpg"
+IMAGE_LOCATION_KNOWN = "images/almaz_office.jpg"
+# IMAGE_LOCATION_UNKNOWN = "images/almaz_bike.jpg"
+IMAGE_LOCATION_UNKNOWN = "images/almazer.jpg"
 IMAGE_SCALE = 200
 
 known_image = face_recognition.load_image_file(IMAGE_LOCATION_KNOWN)
@@ -14,8 +14,12 @@ unknown_image = face_recognition.load_image_file(IMAGE_LOCATION_UNKNOWN)
 known_encoding = face_recognition.face_encodings(known_image)
 unknown_encoding = face_recognition.face_encodings(unknown_image)
 
-face_locations_known = face_recognition.face_locations(face_recognition.load_image_file(IMAGE_LOCATION_KNOWN))
-face_locations_unknown = face_recognition.face_locations(face_recognition.load_image_file(IMAGE_LOCATION_UNKNOWN))
+face_locations_known = face_recognition.face_locations(
+    face_recognition.load_image_file(IMAGE_LOCATION_KNOWN)
+)
+face_locations_unknown = face_recognition.face_locations(
+    face_recognition.load_image_file(IMAGE_LOCATION_UNKNOWN)
+)
 
 results = face_recognition.compare_faces([known_encoding[0]], unknown_encoding[0], 0.5)
 
@@ -33,11 +37,19 @@ class Window(Frame):
         top1, right1, bottom1, left1 = face_locations_known[0]
         top2, right2, bottom2, left2 = face_locations_unknown[0]
 
-        cropped_image_known = image_known.crop([left1 - 20, top1 - 20, right1 + 20, bottom1 + 20])
-        cropped_image_unknown = image_unknown.crop([left2 - 20, top2 - 20, right2 + 20, bottom2 + 20])
+        cropped_image_known = image_known.crop(
+            [left1 - 20, top1 - 20, right1 + 20, bottom1 + 20]
+        )
+        cropped_image_unknown = image_unknown.crop(
+            [left2 - 20, top2 - 20, right2 + 20, bottom2 + 20]
+        )
 
-        cropped_image_known = cropped_image_known.resize((IMAGE_SCALE, IMAGE_SCALE), Image.ANTIALIAS)
-        cropped_image_unknown = cropped_image_unknown.resize((IMAGE_SCALE, IMAGE_SCALE), Image.ANTIALIAS)
+        cropped_image_known = cropped_image_known.resize(
+            (IMAGE_SCALE, IMAGE_SCALE), Image.ANTIALIAS
+        )
+        cropped_image_unknown = cropped_image_unknown.resize(
+            (IMAGE_SCALE, IMAGE_SCALE), Image.ANTIALIAS
+        )
 
         render_known = ImageTk.PhotoImage(cropped_image_known)
         render_unknown = ImageTk.PhotoImage(cropped_image_unknown)
@@ -51,9 +63,14 @@ class Window(Frame):
         label_known.place(x=50, y=50)
         label_unknown.place(x=300, y=50)
 
-        text = Label(width=30, height=2,
-                     text="Это один и тот же человек" if results[0] else "Это разные человеки, хотя похожи",
-                     font="Arial 25", padx=0, pady=0)
+        text = Label(
+            width=30,
+            height=2,
+            text="SAME PERSON" if results[0] else "NOT SAME PERSON",
+            font="Arial 25",
+            padx=0,
+            pady=0,
+        )
         text.pack()
 
 
